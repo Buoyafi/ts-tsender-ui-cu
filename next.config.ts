0d9bbe04be8a7ae5f076e7f6e300443a8440464c
buoyafi <adewunmiyusuf.ay@gmail.com>
2026-01-06 08:09:06 +0100

    my first commit!

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: "",
+  assetPrefix: "./",
+  trailingStash: true
+};
+
+export default nextConfig;
---
+++ src/components/abis/erc20MinimalAbi.ts
@@ -0,0 +1,23 @@
+export const erc20MinimalAbi = [
+    {
+        type: "function",
+        name: "decimals",
+        stateMutability: "view",
+        inputs: [],
+        outputs: [{ name: "", type: "uint8" }],
+    },
+    {
+        type: "function",
+        name: "name",
+        stateMutability: "view",
+        inputs: [],
+        outputs: [{ name: "", type: "string" }],
+    },
+    {
+        type: "function",
+        name: "balanceOf",
+        stateMutability: "view",
+        inputs: [{ name: "account", type: "address" }],
+        outputs: [{ name: "", type: "uint256" }],
+    },
+] as const;
